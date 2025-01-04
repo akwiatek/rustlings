a7a26cd10673b11de5d6ce24d6caae8c4140c74e
Adam Kwiatek <kwiatek.adam@poczta.fm>
2025-01-04 17:46:33 +0100

    exercises

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -2,6 +2,10 @@ fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
 
+    let a = [1; 100];
+
+    println!("a.len: {}", a.len());
+
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
