edb04541eb64bac350736587459e1760323b2570
Adam Kwiatek <kwiatek.adam@poczta.fm>
2025-01-19 18:25:19 +0100

    rustlings

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -1,9 +1,9 @@
 // Lifetimes are also needed when structs hold references.
 
 // TODO: Fix the compiler errors about the struct.
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'author, 'title> {
+    author: &'author str,
+    title: &'title str,
 }
 
 fn main() {
