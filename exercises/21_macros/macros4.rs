7b53c56ae81f11c418820ae0def501ca1a7ef989
Adam Kwiatek <kwiatek.adam@poczta.fm>
2025-02-01 01:51:42 +0100

    rustlings

--- exercises/21_macros/macros4.rs
+++ exercises/21_macros/macros4.rs
@@ -1,15 +1,19 @@
 // TODO: Fix the compiler error by adding one or two characters.
-#[rustfmt::skip]
+//#[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
+    ($val:expr, $val2: expr) => {
+        println!("Look at this other macro: {}, {}", $val, $val2);
+    };
 }
 
 fn main() {
     my_macro!();
     my_macro!(7777);
+    my_macro!(7777, 8888);
 }
